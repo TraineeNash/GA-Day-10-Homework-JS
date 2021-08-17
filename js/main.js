743deb98dc413cdf0456c96d938b8ddafe9851af
Nash_Traineee <55914156+TraineeNash@users.noreply.github.com>
2021-08-18 02:58:56 +0800

    Complete popup model

--- js/main.js
+++ js/main.js
@@ -1,11 +1,65 @@
+let popup = document.querySelector("#popup")
+
+//not recommended. do not know why will ask later
+document.querySelector("#num2").onclick = function(){
+    popup.style.display = "block"
+}
+
 document.querySelector("#num1").addEventListener("click", function(e){
-    console.log("button press")
-    console.log(e);
-    console.log(e.target);
+    popup.style.display = "block"
 })
 
 document.querySelector("#num12").addEventListener("click", function(e){
-    console.log("button 12 press")
-    console.log(e);
-    console.log(e.target);
+    popup.style.display = "block"
+})
+
+document.querySelector("#close").addEventListener("click", function(){
+    popup.style.display = "none"
+})
+
+document.querySelector("#num3").addEventListener("click", function(){
+    popup.style.display = "block"
+})
+
+document.querySelector("#num4").addEventListener("click", function(){
+    popup.style.display = "block"
+})
+
+document.querySelector("#num5").addEventListener("click", function(){
+    popup.style.display = "block"
+})
+
+document.querySelector("#num6").addEventListener("click", function(){
+    popup.style.display = "block"
+})
+
+document.querySelector("#num7").addEventListener("click", function(){
+    popup.style.display = "block"
+})
+
+document.querySelector("#num8").addEventListener("click", function(){
+    popup.style.display = "block"
+})
+
+document.querySelector("#num9").addEventListener("click", function(){
+    popup.style.display = "block"
+})
+
+document.querySelector("#num10").addEventListener("click", function(){
+    popup.style.display = "block"
+})
+
+document.querySelector("#num11").addEventListener("click", function(){
+    popup.style.display = "block"
+})
+
+document.querySelector("#num12").addEventListener("click", function(){
+    popup.style.display = "block"
+})
+
+window.addEventListener("click", function(event){
+    console.log(event.target);
+    if(event.target == popup){
+    popup.style.display = "none"
+    }
 })
